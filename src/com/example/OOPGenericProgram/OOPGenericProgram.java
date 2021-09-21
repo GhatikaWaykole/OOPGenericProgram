66fe96585cd743c66ffb02482f89c6da50b186e2
ghatikawaykole <ghatika99@gmail.com>
2021-09-21 12:34:21 +0530

    Adding the generic method to compute max of any type data

--- src/com/example/OOPGenericProgram/OOPGenericProgram.java
+++ src/com/example/OOPGenericProgram/OOPGenericProgram.java
@@ -3,52 +3,26 @@ package com.example.OOPGenericProgram;
 public class OOPGenericProgram {
     public static void main(String args[]) {
         System.out.println("welcome to generic program");
-        intergerMax(4,2,6);
-        floatMax(1.2f,4.5f,6.8f);
-        stringMax("interger","float","string");
+        compare(4,2,6);
+        System.out.println("float max number" + compare(1.2f,4.2f,6.8f));
+        System.out.println("string max"+compare("interger","float","string"));
     }
     //calling integerMax static method and passing the parameter
 
     /*
-     * method used to find maximum of three inetergr number
+     * generic type method to compare any type of data
      * @param a, b ,c and compareTo method to find the max number
      */
-    public static void intergerMax(Integer a,Integer b,Integer c){
-        int max = a;
+    public static <E extends Comparable<E>> E compare(E a, E b, E c){
+        E max = a;
         if(b.compareTo(max)>0){
             max = b;
         }
         if(c.compareTo(max)>0){
             max = c;
         }
-        System.out.print("integer max"+max);
+        return max;
+
     }
-    /*
-    * floatMax method compraeTo maximum float number
-    * floatMax is given as parametrise method
-     */
-    public static void floatMax(Float a, Float b, Float c){
-        Float max = a;
-        if(b.compareTo(max)>0){
-            max = b;
-        }
-        if(c.compareTo(max)>0){
-         max = c;
-        }
-        System.out.println("flot max"+max);
-    }
-    /*
-   * StringMax method to compute max string
-   * compareto method use to compare the three string
-     */
-    public static void stringMax(String a, String b, String c){
-        String max = a;
-        if(b.compareTo(max)>0){
-            max = b;
-        }
-        if(c.compareTo(max)>0){
-            max = c;
-        }
-        System.out.println("string max"+max);
-    }
+
 }
