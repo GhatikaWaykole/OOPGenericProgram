2069198f4f599d69055a904793686ada80dc92f0
ghatikawaykole <ghatika99@gmail.com>
2021-09-21 12:07:43 +0530

    Adding the comparet to method for interger number

--- src/com/example/OOPGenericProgram/OOPGenericProgram.java
+++ src/com/example/OOPGenericProgram/OOPGenericProgram.java
@@ -3,5 +3,22 @@ package com.example.OOPGenericProgram;
 public class OOPGenericProgram {
     public static void main(String args[]) {
         System.out.println("welcome to generic program");
+        intergerMax(4,2,6);
+    }
+    //calling integerMax static method and passing the parameter
+
+    /*
+     * method used to find maximum of three inetergr number
+     * @param a, b ,c and compareTo method to find the max number
+     */
+    public static void intergerMax(Integer a,Integer b,Integer c){
+        int max = a;
+        if(b.compareTo(max)>0){
+            max = b;
+        }
+        if(c.compareTo(max)>0){
+            max = c;
+        }
+        System.out.print("max"+max);
     }
 }
