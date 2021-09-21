d024131e4f16054e39cfda6bc794fec9c35270a3
ghatikawaykole <ghatika99@gmail.com>
2021-09-21 12:05:29 +0530

    welcome to generic program

--- src/com/example/OOPGenericProgram/OOPGenericProgram.java
+++ src/com/example/OOPGenericProgram/OOPGenericProgram.java
@@ -1,23 +1,7 @@
 package com.example.OOPGenericProgram;
 
 public class OOPGenericProgram {
-    public static void main(String args[]){
-        //calling integerMax static method and passing the parameter
-        intergerMax(4,2,6);
-    }
-    /*
-    * method used to find maximum of three inetergr number
-    * @param a, b ,c and compareTo method to find the max number
-     */
-
-    public static void intergerMax(Integer a,Integer b,Integer c){
-        int max = a;
-        if(b.compareTo(max)>0){
-            max = b;
-        }
-        if(c.compareTo(max)>0){
-            max = c;
-        }
-        System.out.print("max"+max);
+    public static void main(String args[]) {
+        System.out.println("welcome to generic program");
     }
 }
